596163debd657f2f6d6eed1d5c94864d532a6dad
Jovanamaria301 <s59852@caseygrammar.vic.edu.au>
2025-10-16 01:40:54 +0000

    task6

--- Task06.py
+++ Task06.py
@@ -8,7 +8,26 @@ def main():
     #===============================
     # Write your code here
     #-------------------------------
-
+    short_to_full = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "Spetember",
+    "Oct": "October",
+    "Nov": "Novemeber",
+    "Dec": "Decemeber"
+    }
+    short_name = input("Enter a short month name: ")
+    full_name = short_to_full.get(short_name, None)
+    if full_name:
+        print(full_name)
+    else:
+        print("Unrecognized month name")
     #-------------------------------
     # End of your code here
     #===============================
